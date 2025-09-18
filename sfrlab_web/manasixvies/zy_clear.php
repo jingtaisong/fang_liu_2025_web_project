3a6785c3198651e01d15098eaa13bf698a3faef3
Taisong Jing <taisongjing@macbookpro.lan>
2025-09-18 00:47:24 -0700

    Translate zy_clear.php

--- sfrlab_web/manasixvies/zy_clear.php
+++ sfrlab_web/manasixvies/zy_clear.php
@@ -8,7 +8,7 @@
 	}	///////////////////////////
 	$old_upfilk=isset($_POST['old_upfilk']) ? trim($_POST['old_upfilk']) : '';
 	if($upfile1!=$old_upfilk && $upfile1!="")
-	{//是新图
+	{//is a new image
 		$upfile1=$upfile1;
 		//die($old_goods_img);
 		delimgzy($old_upfilk);
@@ -21,7 +21,7 @@
 
 	$old_upfildf=isset($_POST['old_upfildf']) ? trim($_POST['old_upfildf']) : '';
 	if($upfile2!=$old_upfildf && $upfile2!="")
-	{//是新图
+	{//is a new image
 		$upfile2=$upfile2;
 		delimgzy($old_upfildf);
 	}
@@ -33,7 +33,7 @@
 
 	$old_upfil3=isset($_POST['old_upfil3']) ? trim($_POST['old_upfil3']) : '';
 	if($upfil3!=$old_upfil3 && $upfil3!="")
-	{//是新图
+	{//is a new image
 		$upfil3=$upfil3;
 		delimgzy($old_upfil3);	
 	}
@@ -44,7 +44,7 @@
 
 	$old_upfil4=isset($_POST['old_upfil4']) ? trim($_POST['old_upfil4']) : '';
 	if($upfil4!=$old_upfil4 && $upfil4!="")
-	{//是新图
+	{//is a new image
 		$upfil4=$upfil4;
 		delimgzy($old_upfil4);	
 	}
@@ -55,7 +55,7 @@
 
 	$old_upfil5=isset($_POST['old_upfil5']) ? trim($_POST['old_upfil5']) : '';
 	if($upfil5!=$old_upfil5 && $upfil5!="")
-	{//是新图
+	{//is a new image
 		$upfil5=$upfil5;
 		delimgzy($old_upfil5);	
 	}
@@ -66,7 +66,7 @@
 
 	$old_upfil6=isset($_POST['old_upfil6']) ? trim($_POST['old_upfil6']) : '';
 	if($upfil6!=$old_upfil6 && $upfil6!="")
-	{//是新图
+	{//is a new image
 		$upfil6=$upfil6;
 		delimgzy($old_upfil6);	
 	}
@@ -78,7 +78,7 @@
 
 	$old_upfil7=isset($_POST['old_upfil7']) ? trim($_POST['old_upfil7']) : '';
 	if($upfil7!=$old_upfil7 && $upfil7!="")
-	{//是新图
+	{//is a new image
 		$upfil7=$upfil7;
 		delimgzy($old_upfil7);	
 	}
@@ -90,7 +90,7 @@
 	////yansetu
 	$old_upf1=isset($_POST['old_upf1']) ? trim($_POST['old_upf1']) : '';
 	if($upfile1!=$old_upfil1 && $upfile1!="")
-	{//是新图 upfile1
+	{//is a new image upfile1
 		$upfile1=$upfile1;
 		delimgzy($old_upf1);	
 	}
@@ -102,7 +102,7 @@
 
 	$old_upf3=isset($_POST['old_upf3']) ? trim($_POST['old_upf3']) : '';
 	if($upfile3!=$old_upfil6 && $upfile3!="")
-	{//是新图
+	{//is a new image
 		$upfile3=$upfile3;
 		delimgzy($old_upf3);	
 	}
@@ -114,7 +114,7 @@
 
 	$old_upf4=isset($_POST['old_upf4']) ? trim($_POST['old_upf4']) : '';
 	if($upfile4!=$old_upfil6 && $upfile4!="")
-	{//是新图
+	{//is a new image
 		$upfile4=$upfile4;
 		delimgzy($old_upf4);
 	}
@@ -126,7 +126,7 @@
 
 	$old_upf5=isset($_POST['old_upf5']) ? trim($_POST['old_upf5']) : '';
 	if($upfile5!=$old_upfil6 && $upfile5!="")
-	{//是新图
+	{//is a new image
 		$upfile5=$upfile5;
 		delimgzy($old_upf5);	
 	}
@@ -138,7 +138,7 @@
 
 	$old_upf6=isset($_POST['old_upf6']) ? trim($_POST['old_upf6']) : '';
 	if($upfile6!=$old_upfil6 && $upfile6!="")
-	{//是新图
+	{//is a new image
 		$upfile6=$upfile6;
 		delimgzy($old_upf6);	
 	}
@@ -149,7 +149,7 @@
 
 	$old_upf7=isset($_POST['old_upf7']) ? trim($_POST['old_upf7']) : '';
 	if($upfile7!=$old_upfil7 && $upfile7!="")
-	{//是新图
+	{//is a new image
 		$upfile7=$upfile7;
 		delimgzy($old_upf7);
 	}
@@ -160,7 +160,7 @@
 
 	$old_upf8=isset($_POST['old_upf8']) ? trim($_POST['old_upf8']) : '';
 	if($upfile8!=$old_upfil8 && $upfile8!="")
-	{//是新图
+	{//is a new image
 		$upfile8=$upfile8;
 		delimgzy($old_upf8);
 	}
@@ -171,7 +171,7 @@
 
 	$old_upf9=isset($_POST['old_upf9']) ? trim($_POST['old_upf9']) : '';
 	if($upfile9!=$old_upfil9 && $upfile9!="")
-	{//是新图
+	{//is a new image
 		$upfile9=$upfile9;
 		delimgzy($old_upf9);
 	}
@@ -182,7 +182,7 @@
 
 	$old_upf10=isset($_POST['old_upf10']) ? trim($_POST['old_upf10']) : '';
 	if($upfile10!=$old_upfil10 && $upfile10!="")
-	{//是新图
+	{//is a new image
 		$upfile10=$upfile10;
 		delimgzy($old_upf10);	
 	}
@@ -194,7 +194,7 @@
 
 	$old_upf11=isset($_POST['old_upf11']) ? trim($_POST['old_upf11']) : '';
 	if($upfile11!=$old_upfil11 && $upfile11!="")
-	{//是新图
+	{//is a new image
 		$upfile11=$upfile11;
 		delimgzy($old_upf11);	
 	}
